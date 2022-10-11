c2c17bba1fe9da2554a2455b041cc9e8a7b3b273
beboopr <linnets-router.09@icloud.com>
2022-10-11 16:54:27 -0400

    mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,18 +1,34 @@
-import { MongoClient } from "mongodb";
-import {uri } from "./credentials.js";
+import { MongoClient,ObjectId } from "mongodb";
+import { uri } from "./credentials.js";
 
 const client = new MongoClient(uri)
 const db = client.db("sample_mflix")
 const moviesCollection = db.collection("movies")
 
 // 1st way... console.log(await moviesCollection.findOne({}) )
-let query = {title: {$regex: /terminator/i}} // search for "terminator" anywhere in the title and ignores case sensitive
-let firstMovie = await moviesCollection.find(query).limit(3).toArray()
+// let query = { title: { $regex: /terminator/i } } // search for "terminator" anywhere in the title and ignores case sensitive
+// let firstMovie = await moviesCollection.find(query).limit(3).toArray()
 
-for ( let i=0; i < movieArray.length; i++){
-    console.log(movieArray[i].title)
+// for ( let i=0; i < movieArray.length; i++){
+//     console.log(movieArray[i].title)
+// }
+
+// console.log(`there are ${firstMovie.length} movies`)
+// console.log(firstMovie.title)
+
+//add a new movie
+
+const newMovie = {
+    title: "The Boca Code story",
+    Rating: "R",
+    Genre: "Comedy",
+    releaseDate: "2022/12/16",
 }
 
-console.log(`there are ${firstMovie.length} movies`)
-console.log(firstMovie.title)
+// const results = await moviesCollection.insertOne(newMovie)
+// console.log("Results of insert", results)
 
+const updateQuery = { _id: new ObjectId("6345ca7d4eab27f75270d5c6") };
+const update = { $set: {title: "the new bocacode story"}}
+const results = await moviesCollection.findOneAndUpdate(updateQuery,update);
+console.log(results)
